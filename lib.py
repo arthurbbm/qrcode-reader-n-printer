71f498e05374debcc7f1c7ca232ecbb2b7eaab1c
Arthur Machado <arthurbringel@gmail.com>
2023-11-10 14:14:40 -0600

    Team name and year of the trial added to the QR code

--- lib.py
+++ lib.py
@@ -2,7 +2,8 @@ import qrcode
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
 from tempfile import SpooledTemporaryFile, NamedTemporaryFile
-from PIL import Image, ImageWin
+from PIL import Image, ImageWin, ImageDraw, ImageFont
+from textwrap import wrap
 import win32print
 import win32ui
 import win32con
@@ -47,6 +48,14 @@ class BagCode:
         qr.add_data(self.alphanumeric_code)
         qr.make(fit=True)
         img = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(), embeded_image_path=IMG_PATH)
+        draw = ImageDraw.Draw(img)
+        text_group_name = "Soybean Farming Systems"
+        text_trial_year = "2023"
+        font = ImageFont.truetype(
+                ".\\GraphikLight.otf", 20)
+        draw.text((0, 0), text="\n".join(wrap(text_group_name, width=40)), fill=(0, 0, 0), font=font)
+        draw.text((img.pixel_size, 0), text="\n".join(wrap(text_trial_year, width=40)), fill=(0, 0, 0), font=font, anchor="ra")
+
         temp_file = NamedTemporaryFile(delete=True, suffix=".png")
         img.save(temp_file, "PNG")
         temp_file.seek(0)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from lib import BagCode
 if __name__ == "__main__":
     while 1:
         code = BagCode()
-        code.scan_qr_image()
+        # code.scan_qr_image()
+        code.alphanumeric_code = "1234567890"
         code_file = code.generate_reduced_qr()
         code.print_qr_sticker(code_file)
