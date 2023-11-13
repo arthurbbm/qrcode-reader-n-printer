0d6cf3de22b92dbfa74022f2c164bedb916f3474
Arthur Machado <arthurbringel@gmail.com>
2023-11-13 16:17:00 -0600

    Add header to label and resizer QR code to 40% of label height

--- lib.py
+++ lib.py
@@ -3,13 +3,22 @@ from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
 from tempfile import SpooledTemporaryFile, NamedTemporaryFile
 from PIL import Image, ImageWin, ImageDraw, ImageFont
-from textwrap import wrap
 import win32print
 import win32ui
 import win32con
 
 
-IMG_PATH = ".\\mizzouLogo.png"
+dict_replace = {"trial": "Trial",
+                "location": "Location",
+                "plot": "Plot",
+                "time": "Time",
+                "FieldID": "Field ID",
+                "labID": "Lab ID",
+                "variety": "Variety",
+                "pop": "Population",
+                "date": "Date"}
+
+IMG_PATH = "./mizzouLogo.png"
 class BagCode:
     def scan_qr_image(self):
         self.alphanumeric_code = input()
@@ -43,20 +52,68 @@ class BagCode:
         pdc.EndDoc()
         pdc.DeleteDC()
 
-    def generate_reduced_qr(self) -> SpooledTemporaryFile:
-        qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H)
-        qr.add_data(self.alphanumeric_code)
+    def __make_background_image(self, width, height):
+        image = Image.new('RGB', (width, height), 'white')
+        draw = ImageDraw.Draw(image)
+        outer_left, outer_top, outer_right, outer_bottom = 0, 0, width, height
+        draw.rectangle([outer_left, outer_top, outer_right, outer_bottom], fill='white')
+        return image
+
+    def __make_qr_image(self, alphanumeric_code):
+        qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=4, border=0)
+        qr.add_data(alphanumeric_code)
         qr.make(fit=True)
-        img = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(), embeded_image_path=IMG_PATH)
+        img = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer(),
+                            embeded_image_path=IMG_PATH)
+        return img
+
+    def __convert_alphanumeric_to_dict(self, alphanumeric_code):
+        code_dict = {}
+        code_list = alphanumeric_code.split("; ")
+        for code in code_list:
+            key, value = code.split("_")
+            code_dict[key] = value
+        return code_dict
+
+    def __draw_dict_to_image(self, image, code, font_body_size, line_spacing, width, y_text):
+        font_body = ImageFont.truetype(
+            "./Arial Black.ttf", font_body_size)
+        for key, value in code.items():
+            key = dict_replace[key]
+            value = value.capitalize()
+            image.text((10, y_text), key, fill="black", font=font_body)
+            image.text((width // 2, y_text), value, fill="black", font=font_body)
+            y_text += font_body_size + line_spacing
+        return image
+
+    def __make_text_image(self, text, width, height):
+        img = Image.new('RGB', (width, height), 'white')
         draw = ImageDraw.Draw(img)
         text_group_name = "Soybean Farming Systems"
         text_trial_year = "2023"
-        font = ImageFont.truetype(
-                ".\\GraphikLight.otf", 20)
-        draw.text((0, 0), text="\n".join(wrap(text_group_name, width=40)), fill=(0, 0, 0), font=font)
-        draw.text((img.pixel_size, 0), text="\n".join(wrap(text_trial_year, width=40)), fill=(0, 0, 0), font=font, anchor="ra")
+        font_header = ImageFont.truetype(
+                "./Arial Black.ttf", 14)
+        draw.text((3, 0), text=text_group_name, fill=(0, 0, 0), font=font_header)
+        draw.text((3, 15), text=text_trial_year, fill=(0, 0, 0), font=font_header)
+
+        code = self.__convert_alphanumeric_to_dict(text)
+
+        self.__draw_dict_to_image(draw, code, 14, 2, width, 35)
+
+        return img
+    def generate_reduced_qr(self) -> SpooledTemporaryFile:
+        width, height = 216, 384
+        image = Image.new('RGB', (width, height), 'white')
+        background_image = self.__make_background_image(width, height)
+        qr_image = self.__make_qr_image(self.alphanumeric_code)
+        text_image = self.__make_text_image(self.alphanumeric_code, width, int(height/2.5))
+
+        image.paste(background_image, (0, 0))
+        qr_image_offset = int((width - qr_image.pixel_size) / 2), int((height/2.5 + qr_image.pixel_size) / 2)
+        image.paste(qr_image, qr_image_offset)
+        image.paste(text_image, (0, 0))
 
         temp_file = NamedTemporaryFile(delete=True, suffix=".png")
-        img.save(temp_file, "PNG")
+        image.save(temp_file, "PNG")
         temp_file.seek(0)
         return temp_file
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from lib import BagCode
 if __name__ == "__main__":
     while 1:
         code = BagCode()
-        # code.scan_qr_image()
-        code.alphanumeric_code = "1234567890"
+        code.scan_qr_image()
+        # code.alphanumeric_code = "trial_K; location_Carthage; plot_503; time_6; FieldID_4583; labID_4583"
         code_file = code.generate_reduced_qr()
         code.print_qr_sticker(code_file)
